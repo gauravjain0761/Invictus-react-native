ecfd0a109d8b6732ec0538519e2420bb43b8dba2
ritva <112067343+gauravjain0761@users.noreply.github.com>
2023-01-12 15:18:36 +0530

    add chart

--- Src/Themes/Colors.js
+++ Src/Themes/Colors.js
@@ -6,6 +6,7 @@ const Colors = {
   blueOpacityFont: "rgba(0, 37, 58, 0.5)",
   blueOpacity_8Font: "rgba(0, 37, 58, 0.8)",
   placeholderGray: "rgba(106, 112, 129, 0.2)",
+  graphGridLine: "rgba(128,128,128,0.3)",
   textInputBgColor: "#F4F6F8",
   tabIconGray: "#7F8184",
   red: "#EF6A60",
